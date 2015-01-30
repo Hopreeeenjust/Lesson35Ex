889e9fd7b997620fa53f215e1dd8fbfd31f0199c
Roman Jouravkov <roma1990may@rambler.ru>
2015-01-30 15:30:21 +0300

    Initial Commit

---
+++ Lesson35Ex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson35Ex
+//
+//  Created by Hopreeeeenjust on 30.01.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson35Ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson35Ex
+//
+//  Created by Hopreeeeenjust on 30.01.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
